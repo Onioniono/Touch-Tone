0efbd83b60de9d4e9445f2555c169ba44c1b739a
Alexander Mendoza <ajmend005@gmail.com>
2025-12-05 16:40:22 -0800

    Add function to form signals for each digit

---
+++ touchTone.m
@@ -0,0 +1,17 @@
+n = 0:999;
+
+% Digits 0 - 9 as signals of two sinusoids
+function digits = dtmfDigits(n)
+    d0 = sin(0.7217*n) + sin(1.0247*n);
+    d1 = sin(0.5346*n) + sin(0.9273*n);
+    d2 = sin(0.5346*n) + sin(1.0247*n);
+    d3 = sin(0.5346*n) + sin(1.1328*n);
+    d4 = sin(0.5906*n) + sin(0.9273*n);
+    d5 = sin(0.5906*n) + sin(1.0247*n);
+    d6 = sin(0.5906*n) + sin(1.1328*n);
+    d7 = sin(0.6535*n) + sin(0.9273*n);
+    d8 = sin(0.6535*n) + sin(1.0247*n);
+    d9 = sin(0.6535*n) + sin(1.1328*n);
+    
+    digits = {d0,d1,d2,d3,d4,d5,d6,d7,d8,d9};
+end
