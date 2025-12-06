615d92d0626e778c40dea6c82a9035a17bfae63d
Alexander Mendoza <ajmend005@gmail.com>
2025-12-05 16:49:21 -0800

    Add function to turn a phone number into a signal

--- touchTone.m
+++ touchTone.m
@@ -1,7 +1,10 @@
+Fs = 8192;
 n = 0:999;
 
 % Digits 0 - 9 as signals of two sinusoids
 function digits = dtmfDigits(n)
+    % n is the length of samples
+
     d0 = sin(0.7217*n) + sin(1.0247*n);
     d1 = sin(0.5346*n) + sin(0.9273*n);
     d2 = sin(0.5346*n) + sin(1.0247*n);
@@ -14,4 +17,16 @@ function digits = dtmfDigits(n)
     d9 = sin(0.6535*n) + sin(1.1328*n);
     
     digits = {d0,d1,d2,d3,d4,d5,d6,d7,d8,d9};
+end
+
+% Create a phone number signal
+function phone = createPhoneSignal(digits, number)
+    % Digits is from function dtmfDigits()
+    % Number is an array with length of digits
+
+    space = zeros(1,100);   % Empty Space of 100 Samples
+    phone = [];             % Create array for phone number
+    for k = 1:numel(number) % Loop to create phone number
+        phone = [phone digits{number(k)+1} space];
+    end
 end
