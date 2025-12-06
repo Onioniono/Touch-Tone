224b4d2226189314929dd6f19ebc3c52732744df
Alexander Mendoza <ajmend005@gmail.com>
2025-12-05 16:52:11 -0800

    Add function to approximate indeces of dtmf

--- touchTone.m
+++ touchTone.m
@@ -1,4 +1,5 @@
 Fs = 8192;
+N = 2048;
 n = 0:999;
 
 % Digits 0 - 9 as signals of two sinusoids
@@ -29,4 +30,19 @@ function phone = createPhoneSignal(digits, number)
     for k = 1:numel(number) % Loop to create phone number
         phone = [phone digits{number(k)+1} space];
     end
+end
+
+% Approximate indeces for known frequencies
+function [idx_row, idx_col] = dtmfIndices(N)
+    % N is length of samples for fft
+
+    % Row & Columns of Frequencies
+    row = [0.5346 0.5906 0.6535 0.7217];
+    column = [0.9273 1.0247 1.1328];
+    % 0-based indices for row/column tones
+    k_row = round(row*N/(2*pi));
+    k_column = round(column*N/(2*pi));
+    % 1-based indices for row/column tones
+    idx_row = k_row    + 1;         % k = [175 194 214 236]
+    idx_col = k_column + 1;      % k = [303 335 370]
 end
